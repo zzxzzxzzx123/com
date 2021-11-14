86e0b03424137f6024d41d0d068a02744366cecf
张卓欣 <75568989+zzxzzxzzx123@users.noreply.github.com>
2021-11-14 21:09:23 +0800

    Add files via upload

---
+++ src/settings.47c08.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/00-home/home.fire",orientation:"",jsList:["assets/public/js/data/data.f978c.js"],bundleVers:{internal:"279e0",resources:"d5abf",main:"6b583"}};
